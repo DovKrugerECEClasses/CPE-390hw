aca051e02c197668839f3591dce15d7e9f02a59c
Dov Kruger <Dov.Kruger@gmail.com>
2018-10-25 10:44:12 -0400

    updated all homeworks

--- hw2practice_loops/hw2.cc
+++ hw2practice_loops/hw2.cc
@@ -9,16 +9,16 @@ using namespace std;
 	fact(n) should count from 1 to n and multiply all: 1*2*3*...*n
      example: fact(5) = 120
  */
-extern void count(int n); // _Z5counti
+extern void count(int n);     // _Z5counti
 extern void countDown(int n); // _Z9countDowni
-extern int sum(int a, int b);
-extern int fact(int n);
+extern int sum(int a, int b); // _Z3sumii
+extern int fact(int n);       // _Z4facti
 
 int main() {
-	count(10); // count from 1 to 10
-	countDown(10); // count from 10 down to 0
+	count(10); // count from 1 to 10 (no output)
+	countDown(10); // count from 10 down to 0 (no output)
 	cout << sum(3,5) << '\n';
 	for (int i = 0; i < 10; i++)
-		cout << fact(5) << '\n';
+		cout << fact(i) << '\n';
 }
 // 0! = 1, 1! = 1, 2! = 2, 3! = 6, 4! = 24
