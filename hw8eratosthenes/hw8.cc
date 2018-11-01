26faa519caa96ff435a54d6d8f27169170c68e7b
Dov Kruger <Dov.Kruger@gmail.com>
2018-11-01 15:56:32 -0400

    hw8 fixed.  function eratosthenes not done

--- hw8eratosthenes/hw8.cc
+++ hw8eratosthenes/hw8.cc
@@ -19,11 +19,12 @@ using namespace std;
 
  */
 
-extern uint32_t eratosthenes(uint32_t n);
+extern uint32_t eratosthenes(uint32_t* array, uint32_t n);
 
 int main() {
 	uint32_t n = 1000000000;
 	uint32_t* sieve = new uint32_t[(n+63)/64];
 	uint32_t count = eratosthenes(sieve, n);
-	cout << count << '\n';
+	cout << "Primes up to " << n << " = " << count << '\n';
+	delete [] sieve;
 }
